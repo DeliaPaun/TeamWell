87f96376bb182cc6dd78b27354723a64cb3b4536
Delia Paun <paundelia23@stud.ase.ro>
2025-06-17 01:50:35 +0300

    changes

--- teamwell-frontend/src/pages/QuestionnaireList.js
+++ teamwell-frontend/src/pages/QuestionnaireList.js
@@ -77,6 +77,7 @@ export default function QuestionnaireList() {
                   title="Metabase Dashboard"
                   src="https://metabase-production-1670.up.railway.app/public/dashboard/f62d4a0b-f5c7-46cf-8645-98efce443fdb"
                   style={{
+                    position: 'absolute',
                     width: '100%',
                     height: '100%',
                     border: 'none',
